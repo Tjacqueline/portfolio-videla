8a6c4945e514050d3b75c719ab44f4841e1fa9ad
Tjacqueline <vtamarajacqueline@gmail.com>
2022-08-30 19:24:27 -0300

    ultimo cambio: backend

---
+++ portfolio-videla/BackEnd/tjv/src/main/java/com/portfolio/tjv/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.tjv.Repository;
+
+import com.portfolio.tjv.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
---
+++ portfolio-videla/BackEnd/tjv/src/test/java/com/portfolio/tjv/TjvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.tjv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
