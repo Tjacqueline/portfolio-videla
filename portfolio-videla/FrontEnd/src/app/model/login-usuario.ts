5aa6d1cf86e27588c0b832c09c07cdb94d4d5d01
Tjacqueline <vtamarajacqueline@gmail.com>
2022-09-16 16:18:27 -0300

    CLASE jwt-dto EN VSC

---
+++ portfolio-videla/FrontEnd/src/app/model/login-usuario.ts
@@ -0,0 +1,9 @@
+export class LoginUsuario {
+    nombreUsuario: String;
+    password: string;
+
+    constructor(nombreUsuario: String, password: String){
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+    }
+}
--- portfolio-videla/FrontEnd/src/app/model/nuevo-usuario.ts
+++ portfolio-videla/FrontEnd/src/app/model/nuevo-usuario.ts
@@ -1,2 +1,7 @@
 export class NuevoUsuario {
+    nombre!: String;
+    nombreUsuario!: String;
+    email!: String;
+    password!: String;
+    authorities!: String[];
 }
