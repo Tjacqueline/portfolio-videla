151d3de04bdd07697874f3d4679a548b8eeb2c6c
Tjacqueline <vtamarajacqueline@gmail.com>
2022-10-21 17:28:00 -0300

    cambios en el front

--- portfolio-videla/FrontEnd/src/app/service/s-experiencia.service.ts
+++ portfolio-videla/FrontEnd/src/app/service/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-  URL = 'http://localhost:8080/explab/';
+  URL = 'https://tjvbackend.herokuapp.com/explab/';
   constructor(private httpClient: HttpClient) { }
 
   public lista(): Observable<Experiencia[]>{
