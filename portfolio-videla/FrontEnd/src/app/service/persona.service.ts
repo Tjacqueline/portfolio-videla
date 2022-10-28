579267c8eb395a0efed1bcaabefe831ff3621023
Tjacqueline <vtamarajacqueline@gmail.com>
2022-10-28 14:33:13 -0300

    sin dep

--- portfolio-videla/FrontEnd/src/app/service/persona.service.ts
+++ portfolio-videla/FrontEnd/src/app/service/persona.service.ts
@@ -8,7 +8,7 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = 'https://tjvbackend.herokuapp.com/personas/';
+  URL = 'http://localhost:8080/personas/';
 
   constructor(private http: HttpClient) {}
 
--- portfolio-videla/FrontEnd/src/app/service/s-experiencia.service.ts
+++ portfolio-videla/FrontEnd/src/app/service/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-  URL = 'https://tjvbackend.herokuapp.com/explab/';
+  URL = 'http://localhost:8080/explab/';
   constructor(private httpClient: HttpClient) { }
 
   public lista(): Observable<Experiencia[]>{
